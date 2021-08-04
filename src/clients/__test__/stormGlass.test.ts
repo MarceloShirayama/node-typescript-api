98d4325a9905b8a62f4f7d95c6c8330ee83f3f17
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-08-04 19:16:10 -0300

    improved mock axios

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -7,13 +7,16 @@ import {
 jest.mock('axios')
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>
+
   it('Should return the normalized forecast form the StormGlass service', async () => {
     const lat = -33.792726
     const lng = 151.289824
 
-    axios.get = jest.fn().mockResolvedValue({ data: stormglassWeather3Hours })
+    // axios.get = jest.fn().mockResolvedValue({ data: stormglassWeather3Hours })
+    mockedAxios.get.mockResolvedValue({ data: stormglassWeather3Hours })
 
-    const stormGlass = new StormGlass(axios)
+    const stormGlass = new StormGlass(mockedAxios)
     const response = await stormGlass.fetchPoints(lat, lng)
     expect(response).toEqual(stormglassNormalizedResponse3Hours)
   })
